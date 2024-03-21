a689d643f89876984b9a91bccc4748b9b9946a27
sumamashuhaib <sumamashuhaib2@gmail.com>
2024-03-21 15:05:41 +0530

    working 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,52 @@
+
 #include <Arduino.h>
+#include <RTClib.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <RTClib.h>
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
-#include <SPI.h>
- // test for fist commit
-// Uncomment according to your hardware type
-#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-//#define HARDWARE_TYPE MD_MAX72XX::GENERIC_HW
 
-// Defining size, and output pins
+RTC_DS3231 rtc;
+char a = ':';
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
 #define CS_PIN 5
-
 MD_Parola Display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-void setup() {
- 
-  Display.begin();
+
+char daysOfWeek[7][12] = {
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+};
+char buf[20];
+void setup () {
+  Serial.begin(115200);
+
+  if (! rtc.begin()) {
+    Serial.println("RTC module is NOT found");
+    Serial.flush();
+    //while (1);
+  }
+
+  // automatically sets the RTC to the date & time on PC this sketch was compiled
+  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+   //rtc.adjust(DateTime(2024, 3, 21, 3, 0, 0));
+    Display.begin();
   Display.setIntensity(0);
   Display.displayClear();
 }
 
-void loop() {
-  Display.setTextAlignment(PA_LEFT);
-  Display.print("ABHI");
-  delay(2000);
-  Display.print("Afnan");
-  delay(2000);
-  Display.print("Ajay");
-  delay(2000);
-  Display.getScrollSpacing("Vaishnav");
+void loop () {
+  DateTime now = rtc.now();
+  
+   sprintf(buf,"%02d%01c%02d",now.hour(),a, now.minute());
+   Display.print(buf);
+
 }
