0d7510087f044f6eebbfcbad902ebfd55aa816ec
afnan-yusuf <afnanyusufpp@gmail.com>
2024-03-21 20:24:29 +0530

    added task sheduler and millid lib

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <RTClib.h>
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
+#include <millisDelay.h>
+#include <TaskScheduler.h>
 
 RTC_DS3231 rtc;
 char a = ':';
@@ -14,46 +16,43 @@ char a = ':';
 #define CS_PIN 5
 MD_Parola Display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-
 char daysOfWeek[7][12] = {
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-};
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"};
 char buf[20];
-void setup () {
+void setup()
+{
   Serial.begin(115200);
 
-  if (! rtc.begin()) {
+  if (!rtc.begin())
+  {
     Serial.println("RTC module is NOT found");
     Serial.flush();
-    //while (1);
+    // while (1);
   }
-
-  // automatically sets the RTC to the date & time on PC this sketch was compiled
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-
-   //rtc.adjust(DateTime(2024, 3, 21, 3, 0, 0));
-    Display.begin();
+  // rtc.adjust(DateTime(2024, 3, 21, 3, 0, 0));
+  Display.begin();
   Display.setIntensity(0);
-  Display.displayClear();
+  Display.displayText("LED MATRIX CLOCK", PA_CENTER, 100, 2000, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+ // Display.displayClear();
 }
 
-void loop () {
+void loop()
+{
   DateTime now = rtc.now();
   Display.setTextAlignment(PA_CENTER);
-   sprintf(buf,"%02d%01c%02d",now.hour(),a, now.minute());
-   Display.print(buf);
-   delay(5000);
-   sprintf(buf,"%02d%01c%02d",now.day(),a, now.month());
-   Display.print(buf);
-   delay(5000);
-   sprintf(buf,"%04d",now.year());
-   Display.print(buf);
-   delay(5000);
-
+  sprintf(buf, "%02d%01c%02d", now.hour(), a, now.minute());
+  Display.print(buf);
+  delay(5000);
+  sprintf(buf, "%02d%01c%02d", now.day(), a, now.month());
+  Display.print(buf);
+  delay(5000);
+  sprintf(buf, "%04d", now.year());
+  Display.print(buf);
+  delay(5000);
 }
